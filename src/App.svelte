812a1d3309b52cbb409917693b3bdb547a773835
James Geraghty <20022946@mail.wit.ie>
2021-05-15 15:13:03 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import CategoryList from "./CategoryList.svelte"
+
+  const candidateList = [{
+    "name": "Slievenamon",
+
+  }, {
+    "name": "Kilcash Church",
+
+  }];
+</script>
+
+
+
+
+<div class="uk-container">
+  <h1> Donation Client Application </h1>
+  <CategoryList/>
+</div>
---
+++ src/CategoryList.svelte
@@ -0,0 +1,14 @@
+<h3 class="uk-heading-divider">
+    Candidate List
+</h3>
+<div class="uk-table uk-table-divider">
+    <table class="uk-table">
+        <thead>
+        <th>
+            Name
+        </th>
+        </thead>
+        <tbody class="uk-text-left">
+        </tbody>
+    </table>
+</div>
